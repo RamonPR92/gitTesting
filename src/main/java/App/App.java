9737fd80f0f01d6079cd2d23504938b04638d2a8
IDEAR\rperez <ramon.perez@sistemabea.mx>
2019-10-22 12:25:49 -0500

    Feat(Principal): Agregar funcionalidad al metodo principal
    Se agrega funcionalidad al metodo principal iterando un ciclo diez veces e imprimiendo en pantalla el numero.

---
+++ src/main/java/App/App.java
@@ -0,0 +1,11 @@
+package App;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Imprime el numero " + i);
+        }
+    }
+}
