aa586c5062d602cd3d87fd362aab7a57e2efede6
IDEAR\rperez <ramon.perez@sistemabea.mx>
2019-10-22 12:45:58 -0500

    Feat(Principal):Agregar mas funcionalidad al metodo principal
    Se agrega funcionalidad al metodo principal iterando un ciclo diez veces e imprimiendo en pantalla el numero de veces que la mamas.

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -7,5 +7,9 @@ public class App {
         for (int i = 0; i < 10; i++) {
             System.out.println("Imprime el numero " + i);
         }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("No la mames " + i + " veces");
+        }
     }
 }
